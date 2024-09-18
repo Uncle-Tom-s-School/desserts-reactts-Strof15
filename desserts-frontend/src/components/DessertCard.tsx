2f03173468169b14471a93a65d6aab1d3cc16abe
Kristof <robertnagykristof@gmail.com>
2024-09-18 14:17:51 +0200

    Desserts Prop

---
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -0,0 +1,15 @@
+
+type DessertCardProp = {
+    name: string
+}
+
+const DessertCard = (props: DessertCardProp) => {
+  return (
+    <div>
+        {props.name}
+        
+    </div>
+  )
+}
+
+export default DessertCard
