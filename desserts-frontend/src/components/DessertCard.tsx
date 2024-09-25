dd80b888a82d030cb9d9042342c613eb01eab536
Kristof <robertnagykristof@gmail.com>
2024-09-25 14:25:31 +0200

    Main feed complete

--- desserts-frontend/src/components/DessertCard.tsx
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -1,14 +1,25 @@
 import AddToCartBtn from "../AddToCartBtn"
 
-type DessertCardProp = {
+export type DessertCardProp = {
     name: string
+    category : string
+    price : number
+    image : {
+      thumbnail : string,
+      mobile: string,
+      tablet: string,
+      desktop: string
+    }
 }
 
 const DessertCard = (props: DessertCardProp) => {
   return (
-    <div>
-        {props.name}
+    <div className="dessert-card">
+      <img src={props.image.desktop}></img>
         <AddToCartBtn/>
+        <h4>{props.category}</h4>
+        <h2>{props.name}</h2>
+        <h3>{props.price}</h3>
     </div>
   )
 }
